123ba0fa43db3023986bc70915c8018ac3e88eee
Andrei Rus <andrei.rus@btrl.ro>
2022-06-21 11:01:33 +0300

    Initial commit

---
+++ src/main/java/com/example/earthquake_s2/EarthquakeS2Application.java
@@ -0,0 +1,13 @@
+package com.example.earthquake_s2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EarthquakeS2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EarthquakeS2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/earthquake_s2/EarthquakeS2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.earthquake_s2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EarthquakeS2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
